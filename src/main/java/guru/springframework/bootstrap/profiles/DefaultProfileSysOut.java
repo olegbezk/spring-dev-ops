8ff1415f409ed1a1700fc98bd472d3e56c504f26
Oleg Bezkorovaynyi <Oleg.Bezkorovaynyi@Sophos.com>
2018-03-19 22:06:30 +0200

    spring boot profile properties

--- src/main/java/guru/springframework/bootstrap/profiles/DefaultProfileSysOut.java
+++ src/main/java/guru/springframework/bootstrap/profiles/DefaultProfileSysOut.java
@@ -1,5 +1,7 @@
 package guru.springframework.bootstrap.profiles;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
@@ -7,10 +9,12 @@ import org.springframework.stereotype.Component;
 @Profile("default")
 public class DefaultProfileSysOut {
 
-    public DefaultProfileSysOut() {
+    @Autowired
+    public DefaultProfileSysOut( @Value("${custom.springframework.profile.message}") String message) {
         System.out.println("#########################");
         System.out.println("#########################");
         System.out.println("###      DEFAULT      ###");
+        System.out.println(message);
         System.out.println("#########################");
         System.out.println("#########################");
     }
