4eafc801ae13d1469ac577be924ba6059edb76fc
Oleg Bezkorovaynyi <Oleg.Bezkorovaynyi@Sophos.com>
2018-03-19 16:32:22 +0200

    refactor to standalone package

--- src/test/java/guru/springframework/test/config/SpringBootJavaConfig.java
+++ src/test/java/guru/springframework/test/config/SpringBootJavaConfig.java
@@ -1,4 +1,4 @@
-package guru.springframework.test;
+package guru.springframework.test.config;
 
 import guru.springframework.test.jms.FakeJmsBroker;
 import org.springframework.beans.factory.annotation.Value;
